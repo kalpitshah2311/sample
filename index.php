50b47d361148e1b2a82d339e0f6f0c5a2cdcf390
Kalpit Shah <72689203+kalpitshah2311@users.noreply.github.com>
2022-03-27 18:48:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-<?php
+<html>
+  <head>
+    <body>
+      <h1>
+        <?php
 
 echo "bro mujse zyada expect mat kar mere ko itna hi aata hai";
 ?>
+      </h1>
+    </body>
+  </head>
+</html>
+
